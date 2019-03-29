16f965192d4cbe335ff65826a92ae4c486697b73
Nguyễn Danh Nam <namnguyen1761998@gmail.com>
2019-03-29 11:36:39 +0700

    fix static dir

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/HRMana-WebappAngular'));
+app.use(express.static(__dirname + '/dist/webapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/HRMana-WebappAngular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/webapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
